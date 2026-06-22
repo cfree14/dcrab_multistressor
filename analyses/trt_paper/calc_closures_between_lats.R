fc10d7c90b58700b9926d44bbdca583d8d9ca2cc
cfree14 <cfree14@gmail.com>
2026-06-22 14:18:07 -0700

    work

--- analyses/trt_paper/calc_closures_between_lats.R
+++ analyses/trt_paper/calc_closures_between_lats.R
@@ -15,7 +15,7 @@ outdir <- "analyses/trt_paper/output"
 plotdir <- "trt_paper/figures"
 
 # Read data
-data_orig <- readRDS("/Users/cfree/Dropbox/Chris/UCSB/projects/domoic_closures/data/processed/2015_2023_WC_dcrab_closures.Rds")
+data_orig <- readRDS("/Users/cfree/Dropbox/Chris/UCSB/projects/domoic_closures/data/processed/2015_2024_WC_dcrab_closures.Rds")
 
 # Read time key
 time_key_orig <- read.csv(file.path(indir, "days_per_period.csv")) 
@@ -30,43 +30,91 @@ time_key <- time_key_orig %>%
   # Reduce
   filter(min_date > "2015-12-13")
 
-# Latitude breaks
-lat_breaks <- c(32,
-                34+27/60,
-                36,
-                37+11/60,
-                38+46.125/60,
-                40+10/60,
-                42,
-                # Oregon
-                43.12, # Bandon
-                45.06, # Cascade Head
-                46.263, # OR/WA border
-                # Washington
-                46.466, # Klipsan Beach
-                47.67, # Destruction Island
-                48.29 # Tatoosh Island
-)
+# If Jameal version
+version <- "real"
+# version <- "jameal"
+if(version=="jameal"){
 
-# Format data
-data <- data_orig  %>% 
-  # Remove above max lat considered
-  filter(lat_dd<=max(lat_breaks)) %>% 
-  # Add lat bin
-  mutate(lat_bin=cut(lat_dd, breaks=lat_breaks, labels=c("CA6", "CA5", "CA4", "CA3", "CA2", "CA1",
-                                                         "OR1", "OR2", "OR3",
-                                                         "WA1", "WA2", "WA3")))
-
-
-# Visualize intersection of weeks/zones with closure
-################################################################################
-
-
-
+  # Latitude breaks
+  lat_breaks <- c(32,
+                  34+27/60,
+                  36,
+                  37+11/60,
+                  38+46.125/60,
+                  40+10/60,
+                  42,
+                  # Oregon
+                  43.12, # Bandon
+                  45.06, # Cascade Head
+                  46.263, # OR/WA border
+                  # Washington
+                  46.466, # Klipsan Beach
+                  47.67, # Destruction Island
+                  48.29 # Tatoosh Island
+  )
+  
+  # Format data
+  data <- data_orig  %>% 
+    # Remove above max lat considered
+    filter(lat_dd<=max(lat_breaks)) %>% 
+    # Add lat bin
+    mutate(lat_bin=cut(lat_dd, breaks=lat_breaks, labels=c("CA6", "CA5", "CA4", "CA3", "CA2", "CA1",
+                                                           "OR1", "OR2", "OR3",
+                                                           "WA1", "WA2", "WA3")))
+  
+}else{
+  
+  # Latitude breaks
+  lats_orig <- readxl::read_excel("data/mgmt_areas/WC_dcrab_da_mgmt_zones.xlsx") %>% filter(state!="California")
+  lat_breaks <- c(lats_orig$lat_dd_north, lats_orig$lat_dd_south,
+                  32,
+                  34+27/60,
+                  36,
+                  37+11/60,
+                  38+46.125/60,
+                  40+10/60,
+                  42) %>% sort() %>% unique() %>% na.omit()
+  lat_zones <- c(paste0("CA-", 6:1), 
+                 paste0("OR-50-", LETTERS[12:1]), 
+                 c("WA-60A-2", "WA-60A-2", "WA-59A-2", "WA-59A-1"))
+  
+  data <- data_orig  %>% 
+    # Remove above max lat considered
+    filter(lat_dd<=max(lat_breaks)) %>% 
+    # Add lat bin
+    mutate(lat_bin=cut(lat_dd, breaks=lat_breaks, labels=lat_zones))
+  
+}
 
 
 # Loop through time key and calculate closed days by lat zone
 ################################################################################
+
+all_statuses <- sort(unique(data_orig$status))
+
+# Status
+open_status <- c("Season open")
+closed_status <- c("Out-of-season",
+                   "Body condition delay", 
+                   "Body condition/domoic acid delay",                   
+                   "Domoic acid delay",
+                   "Whale entanglement closure",
+                   "Whale entanglement/domoic acid delay")
+restriction_status <- c("Evisceration order",
+                        "Evisceration order (+depth restriction/gear reduction)",
+                        "30-fathom depth restriction",                          
+                        "40-fathom depth restriction",      
+                        "40-fathom depth restriction/20% gear reduction",  
+                        "30-fathom depth restriction/25% gear reduction",
+                        "30-fathom depth restriction/50% gear reduction",
+                        "25% gear reduction",
+                        "33% gear reduction",                              
+                        "50% gear reduction")
+merged_statuses <- c(open_status, closed_status, restriction_status)
+
+# Check
+all_statuses[!all_statuses %in% merged_statuses]
+
 
 # Loop through
 row_ids <- time_key$row_id
@@ -79,7 +127,7 @@ stats <- purrr::map_df(row_ids, function(x){
   date2 <- time_row$max_date
   
   # Filter closure data
-  sdata <- data_orig %>% 
+  sdata <- data %>% 
     # Reduce to period between dates
     filter(date>=date1 & date<=date2) %>% 
     # Summarize number of lat-days with closures
@@ -87,9 +135,13 @@ stats <- purrr::map_df(row_ids, function(x){
     summarize(ndays=n_distinct(date),
               lat_range=max(lat_dd)-min(lat_dd),
               latdays_tot=n()*0.01,
-              latdays_close=sum(status!='Season open') * 0.01) %>% 
+              latdays_open=sum(status %in% open_status) * 0.01,
+              latdays_restricted=sum(status %in% restriction_status) * 0.01,
+              latdays_closed=sum(status %in% closed_status) * 0.01,) %>% 
     ungroup() %>% 
-    mutate(latdays_close_prop=latdays_close/latdays_tot) %>% 
+    mutate(latdays_open_prop=latdays_open/latdays_tot,
+           latdays_restricted_prop=latdays_restricted/latdays_tot,
+           latdays_closed_prop=latdays_closed/latdays_tot) %>% 
     # Add row id
     mutate(row_id=x)
   
@@ -104,14 +156,23 @@ stats_out <- stats %>%
   # Add time key meta-data
   left_join(time_key) %>% 
   # Arrange
-  select(row_id, crab_year, period, n_days, min_date, max_date, everything())
+  select(row_id, crab_year, period, n_days, min_date, max_date, everything()) %>% 
+  # Check sum
+  mutate(prop_check=latdays_open_prop+latdays_restricted_prop+latdays_closed_prop)
   
+# Should all be 1
+table(stats_out$prop_check)
+
 
 # Export
 ################################################################################
 
+outfile <- ifelse(version=="real",
+                  "percent_of_zones_under_restriction_by_week_and_zone_real.csv",
+                  "percent_of_zones_under_restriction_by_week_and_zone_jameal.csv")
+
 # Export
-write.csv(stats_out, file=file.path(outdir, "percent_of_zones_under_restriction_by_week_and_zone.csv"), row.names = F)
+write.csv(stats_out, file=file.path(outdir, outfile), row.names = F)
 
 
 
---
+++ analyses/trt_paper/code/compare_real_zones_against_closures.R
@@ -0,0 +1,141 @@
+
+# Clear workspace
+rm(list = ls())
+
+# Setup
+################################################################################
+
+# Packages
+library(tidyverse)
+library(lubridate)
+
+# Directories
+indir <- "analyses/trt_paper/data"
+outdir <- "analyses/trt_paper/output"
+plotdir <- "analyses/trt_paper/figures"
+
+# Read data
+data_orig <- readRDS("/Users/cfree/Dropbox/Chris/UCSB/projects/domoic_closures/data/processed/2015_2024_WC_dcrab_closures.Rds")
+
+# Read time key
+time_key_orig <- read.csv(file.path(indir, "days_per_period.csv")) 
+
+# Latitude breaks
+lats_orig <- readxl::read_excel("data/mgmt_areas/WC_dcrab_da_mgmt_zones.xlsx")
+lat_breaks <- c(lats_orig$lat_dd_north, lats_orig$lat_dd_south) %>% sort() %>% unique() %>% na.omit()
+
+# Build data
+################################################################################
+
+# Levels
+sort(unique(data_orig$status))
+
+# Order levels (use ORIGINAL name)
+levels_use <- c("Season open",                                           
+                "Out-of-season",   
+                "Body condition delay",                                   
+                "Body condition/domoic acid delay", 
+                "Domoic acid delay",                                     
+                "Evisceration order",                                    
+                "Evisceration order (+depth restriction/gear reduction)", 
+                "Whale entanglement/domoic acid delay",
+                "Whale entanglement closure",
+                "30-fathom depth restriction",   
+                "40-fathom depth restriction", 
+                "25% gear reduction",  
+                "33% gear reduction",                                    
+                "50% gear reduction",
+                "40-fathom depth restriction/20% gear reduction",
+                "30-fathom depth restriction/25% gear reduction",         
+                "30-fathom depth restriction/50% gear reduction")
+
+# Reset order
+data <- data_orig %>%
+  # Ordeer
+  mutate(status = factor(status, levels = levels_use)) %>% 
+  # Update level name
+  mutate(status = fct_recode(
+    status,
+    "Evisceration order (+depth/gear restriction)" = "Evisceration order (+depth restriction/gear reduction)"
+  ))
+
+# Inspect
+str(data)
+#freeR::complete(data1)
+levels(data$status)
+table(data$status)
+
+# Plot data
+################################################################################
+
+# Sonoma-Mendocino county line
+son_mend_county <- 38+46.125/60
+date_min_do <- min(data_orig$date)
+
+# Theme
+my_theme <-  theme(axis.text=element_text(size=7),
+                   axis.title=element_text(size=8),
+                   legend.text=element_text(size=7),
+                   legend.title=element_text(size=8),
+                   strip.text=element_text(size=8),
+                   plot.title=element_blank(),
+                   # Gridlines
+                   panel.grid.major = element_blank(),
+                   panel.grid.minor = element_blank(),
+                   panel.background = element_blank(),
+                   axis.line = element_line(colour = "black"),
+                   # Legend
+                   legend.key.size = unit(0.4, "cm"),
+                   legend.background = element_rect(fill=alpha('blue', 0)))
+
+# Plot data
+g <- ggplot(data, aes(x=date, y=lat_dd, fill=status)) +
+  # Plot raster
+  geom_raster() +
+  # Jameal zones
+  geom_hline(yintercept=lat_breaks, linewidth=0.2, linetype="dotted") +
+  # State/region lines
+  geom_hline(yintercept=c(48.43333, 46.25000, 42.00000), linewidth=0.2) +
+  geom_hline(yintercept = son_mend_county, linetype="dashed", linewidth=0.2) + # Sonoma/Mendocino
+  # Week lines
+  # geom_vline(xintercept = lubridate::ymd(time_key_orig$min_date),
+  #            linewidth=0.2) +
+  # Label state lines
+  annotate(geom="text", x=date_min_do, y=48.48, hjust=0, vjust=1.5, label="Washington", color="black", size=2.5) +
+  annotate(geom="text", x=date_min_do, y=46.25, hjust=0, vjust=1.5, label="Oregon", color="black", size=2.5) +
+  annotate(geom="text", x=date_min_do, y=42, hjust=0, vjust=1.5, label="N. California", color="black", size=2.5) +
+  annotate(geom="text", x=date_min_do, y=son_mend_county, hjust=0, vjust=1.5, label="C. California", color="black", size=2.5) +
+  # Axis
+  scale_x_date(date_breaks="1 year", date_labels = "%Y") +
+  scale_y_continuous(breaks=35:48, lim=c(35,NA)) +
+  # Labels
+  labs(x="Date", y="Latitude (°N)") +
+  # Legends
+  scale_fill_manual(name="Season status", 
+                    values=c("grey85", # Season open
+                             "white", # Out-of-season
+                             "pink", # Body condition delay
+                             "orange", # Body condition/domoic acid delay
+                             "darkred", # Domoic acid delay
+                             "coral", # Evisceration order
+                             "violetred", # Evisceration order (+depth/gear restriction)
+                             "purple3", # Whale entanglement/domoic acid delay
+                             "navy", # Whale entanglement closure
+                             "dodgerblue3", # 30-fathom depth restriction
+                             "dodgerblue1", # 40-fathom depth restriction
+                             "lightblue1", # 25% gear reduction 
+                             "lightblue2", # 33% gear reduction
+                             "lightblue3", # 50% gear reduction
+                             "springgreen1",  # 30-fathom depth restriction/25% gear reduction
+                             "springgreen3",  # 30-fathom depth restriction/50% gear reduction
+                             "springgreen4" # 40-fathom depth restriction/20% gear reduction
+                             ),
+                    drop=F) +
+  # Theme
+  theme_bw() + my_theme
+g
+
+# Export plot
+ggsave(g, filename=file.path(plotdir, "dcrab_season_status_by_real_zones.png"),
+       width=6.5, height=3.25, units="in", dpi=600)
+
